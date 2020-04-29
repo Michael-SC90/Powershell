eeef96d0b583e2a6cd47405128066120a83ea0fa
Michael Craig <34893491+Michael-SC90@users.noreply.github.com>
2020-04-29 10:25:22 -0700

    Delete logPurge.ps1

--- Logging/logPurge.ps1
+++
@@ -1,8 +0,0 @@
-﻿Function Purge-Logs() {
-    param(
-        [String] $Path = $(Throw "Log Folder Path must be specified."),
-        [Int] $DaysOld = 90
-    )
-
-    gci $path -Include '*.log' -Recurse | ? LastWriteTime -LT (Get-Date).AddDays(-$DaysOld) | Remove-Item
-}
